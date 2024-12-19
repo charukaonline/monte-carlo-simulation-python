fb340782e3d886a532a6f10fa28e4e5a3357eb43
charukaonline <nuwanthakonline@gmail.com>
2024-12-19 23:00:34 +0530

    #

--- final.py
+++ final.py
@@ -17,11 +17,13 @@ def drop_marbles(num_drops, radius=1):
         # Generate random point within the table
         x, y = random.uniform(-4, 2), random.uniform(-2, 2)
 
-        # Check if the point falls inside the circular tray (radius = 1)
+        # Check if the point falls inside the circular tray
+        # radius = 1, center = (0, 0)
         if x**2 + y**2 <= radius**2:
             circular_count += 1
 
-        # Check if the point falls inside the rectangular tray (side length = radius)
+        # Check if the point falls inside the rectangular (square shape) tray
+        # (side length = radius) with positioned at (-2.5, 0)
         if -3 <= x <= -2 and -0.5 <= y <= 0.5:
             rectangular_count += 1
 
